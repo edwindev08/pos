b78eaf1cc947adc25844a8d1880ec7802c97bc33
jeybeatz <podg08@gmail.com>
2021-04-18 15:06:24 -0500

    Initial Commit

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2021 <a href="https://www.tech2life.venndelo.com" target="_blank">Tech2Life</a>.</strong>
+
+	Todos los derechos reservados.
+
+
+</footer>
