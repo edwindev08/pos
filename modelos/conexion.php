b221a6252ad81d4fa5ad5342a6e701e04b08c221
jeybeatz <podg08@gmail.com>
2022-05-27 03:45:42 -0500

    Base de datos OK en Heroku

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -6,9 +6,9 @@ class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=us-cdbr-east-05.cleardb.net;dbname=heroku_7e1d733973ac94e",
-			            "bcc24b0b0aff61",
-			            "c7549968");
+		$link = new PDO("mysql:host=us-cdbr-east-05.cleardb.net;dbname=heroku_ca100350e92d663",
+			            "b48548ca76ce24",
+			            "847ba98a");
 
 		$link->exec("set names utf8");
 
@@ -17,3 +17,4 @@ class Conexion{
 	}
 
 }
+
